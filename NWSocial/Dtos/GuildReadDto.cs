f2580d9ade16095052c219401fe717ea8c6583d4
Valentin P <38946427+zedow@users.noreply.github.com>
2020-10-08 23:50:57 +0200

    Guild To User relation

--- NWSocial/Dtos/GuildReadDto.cs
+++ NWSocial/Dtos/GuildReadDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NWSocial.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,5 +12,6 @@ namespace NWSocial.Dtos
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public List<UserReadDto> Users { get; set; }
     }
 }
