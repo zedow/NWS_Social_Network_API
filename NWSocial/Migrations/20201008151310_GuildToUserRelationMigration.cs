d0fa529dc06f43642fa6b075b6a7d2a3975ff769
Valentin P <38946427+zedow@users.noreply.github.com>
2020-10-08 17:27:34 +0200

    User To Guild relation
    Migration de la base de données

---
+++ NWSocial/Migrations/20201008151310_GuildToUserRelationMigration.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NWSocial.Migrations
+{
+    public partial class GuildToUserRelationMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_guilds",
+                table: "guilds");
+
+            migrationBuilder.RenameTable(
+                name: "guilds",
+                newName: "Guilds");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Guilds",
+                table: "Guilds",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGuild",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false),
+                    GuildId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGuild", x => new { x.UserId, x.GuildId });
+                    table.ForeignKey(
+                        name: "FK_UserGuild_Guilds_GuildId",
+                        column: x => x.GuildId,
+                        principalTable: "Guilds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserGuild_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGuild_GuildId",
+                table: "UserGuild",
+                column: "GuildId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserGuild");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Guilds",
+                table: "Guilds");
+
+            migrationBuilder.RenameTable(
+                name: "Guilds",
+                newName: "guilds");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_guilds",
+                table: "guilds",
+                column: "Id");
+        }
+    }
+}
