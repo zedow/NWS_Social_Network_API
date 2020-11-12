bde9d8a66bd8184082652fb31dcc112c02928425
Valentin P <38946427+zedow@users.noreply.github.com>
2020-11-12 20:35:10 +0100

    Basic model

---
+++ NWSocial/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace NWSocial.Models
+{
+    public class Project
+    {
+        [Key]
+        public int Id { get; set; }
+        public int? GuildId { get; set; }
+    }
+}
